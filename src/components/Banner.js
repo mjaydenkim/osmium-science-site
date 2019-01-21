1259d36b33de16cb311fb33d6b199b247c62d8bf
Matthew Kim <mjaydenkim@gmail.com>
2019-01-20 16:44:24 -0800

    Update src/components/Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,7 +7,7 @@ const Banner = (props) => (
                 <h1>Hi.</h1>
             </header>
             <div className="content">
-                <p>A responsive site template designed by HTML5 UP<br />
+                <p>A blog created by Matthew Kim.<br />
                 and released under the Creative Commons.</p>
                 <ul className="actions">
                     <li><a href="#one" className="button next scrolly">Get Started</a></li>
