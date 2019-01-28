f48817c413c7e2c7240e8df032b22e3900d8a4a5
mjaydenkim <mjaydenkim@gmail.com>
2019-01-28 11:29:40 -0800

    Update generic.js

--- src/pages/generic.js
+++ src/pages/generic.js
@@ -18,10 +18,7 @@ const Generic = (props) => (
                         <h1>Generic</h1>
                     </header>
                     <span className="image main"><img src={pic11} alt="" /></span>
-                    <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
-                    <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit tristique lorem ipsum dolor.</p>
-                    <iframe src="https://docs.google.com/a/lasdk8.org/document/d/e/2PACX-1vQSKgVJhB4w60UxHLgyk8CQwU44np3WkOT6tDHM8gQ_BAlSH_YnhH7N6umfYxBeSF8dLNgDYTT8VNlZ/pub?embedded=true"></iframe>
+                    <iframe src="https://docs.google.com/a/lasdk8.org/document/d/e/2PACX-1vQSKgVJhB4w60UxHLgyk8CQwU44np3WkOT6tDHM8gQ_BAlSH_YnhH7N6umfYxBeSF8dLNgDYTT8VNlZ/pub?embedded=true" width = 1200 height = 800></iframe>
                 </div>
             </section>
         </div>
